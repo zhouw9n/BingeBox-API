06326d4858371fa35f97783b0f6cab48a8aabf45
zhouw9n <76569752+zhouw9n@users.noreply.github.com>
2025-05-31 17:07:39 +0200

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -134,7 +134,7 @@ app.post("/api/embed", async (req, res) => {
             }
         );
 
-        const embeddings = response.embeddings;
+        const embeddings = response.embeddings.float;
 
         const vectorLength = embeddings[0].length;
         const averageEmbedding = Array(vectorLength).fill(0);
