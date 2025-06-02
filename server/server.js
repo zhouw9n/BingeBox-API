b205762e1744b56f668b650b78f1288714cac5b4
zhouw9n <76569752+zhouw9n@users.noreply.github.com>
2025-06-02 14:54:10 +0200

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -136,7 +136,7 @@ app.post("/api/datastrax/library", async (req, res) => {
             }   
         );
         const data = await response.json();
-        return res.json(data);
+        res.json(data);
     } catch (error) {
         res.status(500).json({error: error.message});
     }
